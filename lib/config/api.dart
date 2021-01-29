95014144eff128de9f31e05ea7baeead13133f53
Hector Hernandez <hector.hvelozo@gmail.com>
2021-01-29 11:30:47 -0500

    Update API URL

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,5 @@
 
-final String _path = "https://panel2.llega.net/"; 
+final String _path = "https://panel.llega.net/"; 
 String serverPath = "${_path}api/";
 String serverImages = "${_path}images/";
 String serverImgNoUserPath = "${_path}img/user.png";
